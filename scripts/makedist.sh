fb60a1a4fe74b3c0a62b496afb852bf4deaa3f34
Remco Gerlich <remco.gerlich@nelen-schuurmans.nl>
2018-04-04 12:17:43 +0200

    Fix release scripts

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -5,8 +5,9 @@ DIRECTORY=`pwd`
 
 mkdir -p /tmp/dist
 cp -r $DIRECTORY/build/* /tmp/dist/
-cd /tmp
+cd /tmp/dist
 mkdir -p $DIRECTORY/dist
-zip -r $DIRECTORY/dist/lizard-management-client.zip dist
+zip -r $DIRECTORY/dist/lizard-management-client.zip .
+cd /
 rm -r /tmp/dist
 ls -l $DIRECTORY/dist/lizard-management-client.zip
