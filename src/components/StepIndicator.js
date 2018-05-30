23bef419e1c30cdf2e78bb9868361c33b6eb0fd3
Gijs Nijholt <gijs.nijholt@gmail.com>
2018-05-30 09:07:12 +0200

    Alarm Wizard: Back to previous steps (#19)
    * Add cursor:pointer and onclick prop to stepindicator component
    
    * Adds goBackToStep(step)
    
    * Open support PDF in new window

--- src/components/StepIndicator.js
+++ src/components/StepIndicator.js
@@ -3,9 +3,10 @@ import styles from "./StepIndicator.css";
 
 class StepIndicator extends Component {
   render() {
-    const { indicator, active } = this.props;
+    const { indicator, active, handleClick } = this.props;
     return (
       <div
+        onClick={handleClick}
         className={`${styles.StepIndicator} ${active
           ? styles.Active
           : styles.Inactive}`}
