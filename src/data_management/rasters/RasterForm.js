b029a140ccb10c4faf993be1e288d6c2d3c4f789
Lirry Pinter <lirry.pinter@nelen-schuurmans.nl>
2018-12-06 16:03:24 +0100

    remove information icons from new raster form

--- src/data_management/rasters/RasterForm.js
+++ src/data_management/rasters/RasterForm.js
@@ -500,9 +500,7 @@ class RasterFormModel extends Component {
         <div className={gridStyles.Container}>
           <div className={`${gridStyles.Row}`}>
             <div
-              className={`${gridStyles.colLg12} ${gridStyles.colMd12} ${
-                gridStyles.colSm12
-              } ${gridStyles.colXs12}`}
+              className={`${gridStyles.colLg12} ${gridStyles.colMd12} ${gridStyles.colSm12} ${gridStyles.colXs12}`}
             >
               <div id="steps" style={{ margin: "20px 0 0 20px" }}>
                 <GenericTextInputComponent
@@ -610,42 +608,34 @@ class RasterFormModel extends Component {
                   choices={[
                     {
                       display: "none",
-                      description: "",
-                      info: "no aggregation available for this raster"
+                      description: ""
                     },
                     {
                       display: "counts",
-                      description: "means percentage per category",
-                      info: "means percentage per category"
+                      description: "means percentage per category"
                     },
                     {
                       display: "curve",
-                      description: "means percentile curve",
-                      info: "means percentile curve"
+                      description: "means percentile curve"
                     },
                     {
                       display: "histogram",
-                      description: "means frequency per data band",
-                      info: "means frequency per data band"
+                      description: "means frequency per data band"
                     },
                     {
                       display: "sum",
-                      description: "",
-                      info: "sum"
+                      description: ""
                     },
                     {
                       display: "average",
-                      description: "",
-                      info: "average"
+                      description: ""
                     }
                   ]}
                   transformChoiceToDisplayValue={item => item.display}
                   transformChoiceToDescription={item => item.description}
-                  transformChoiceToInfo={item => item.info}
                   modelValue={aggregationType} // string: e.g. the name of a raster
                   updateModelValue={item =>
-                    this.setAggregationType(item.display)
-                  } // cb function to *update* the value of e.g. a raster's name in the parent model
+                    this.setAggregationType(item.display)} // cb function to *update* the value of e.g. a raster's name in the parent model
                   resetModelValue={() => this.setAggregationType("")} // cb function to *reset* the value of e.g. a raster's name in the parent model
                   validate={this.validateAggregationType} // cb function to validate the value of e.g. a raster's name in both the parent model as the child compoennt itself.
                 />
@@ -868,9 +858,7 @@ class RasterFormModel extends Component {
                   <div className={inputStyles.InputContainer}>
                     <button
                       type="button"
-                      className={`${buttonStyles.Button} ${
-                        buttonStyles.Success
-                      }`}
+                      className={`${buttonStyles.Button} ${buttonStyles.Success}`}
                       style={{ marginTop: 10 }}
                       onClick={() => {
                         this.handleClickCreateRaster();
@@ -918,10 +906,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 };
 
 const RasterForm = withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(RasterFormModel)
+  connect(mapStateToProps, mapDispatchToProps)(RasterFormModel)
 );
 
 export { RasterForm };
