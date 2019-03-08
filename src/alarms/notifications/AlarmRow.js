9f3218a6c2d469d02bd6fcffd1112cf96de855c7
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-03-08 11:14:41 +0100

    remove commented out code

--- src/alarms/notifications/AlarmRow.js
+++ src/alarms/notifications/AlarmRow.js
@@ -13,65 +13,7 @@ class AlarmRow extends Component {
       isFetching: true,
       isActive: props.alarm.active
     };
-    // this.activateAlarm = this.activateAlarm.bind(this);
-    // this.deActivateAlarm = this.deActivateAlarm.bind(this);
   }
-
-  // activateAlarm(uuid) {
-  //   const { addNotification } = this.props;
-
-  //   fetch(`/api/v3/rasteralarms/${uuid}/`, {
-  //     credentials: "same-origin",
-  //     method: "PATCH",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       active: true
-  //     })
-  //   })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       this.setState({
-  //         isActive: true
-  //       });
-  //       addNotification(`Alarm activated`, 2000);
-  //     });
-  // }
-
-  // deActivateAlarm(uuid) {
-  //   const { addNotification } = this.props;
-
-  //   fetch(`/api/v3/rasteralarms/${uuid}/`, {
-  //     credentials: "same-origin",
-  //     method: "PATCH",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       active: false
-  //     })
-  //   })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       this.setState({
-  //         isActive: false
-  //       });
-  //       addNotification(`Alarm deactivated`, 2000);
-  //     });
-  // }
-
-  // removeAlarm(uuid) {
-  //   const { loadAlarmsOnPage, addNotification } = this.props;
-
-  //   fetch(`/api/v3/rasteralarms/${uuid}/`, {
-  //     credentials: "same-origin",
-  //     method: "DELETE",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({})
-  //   }).then(response => {
-  //     if (response.status === 204) {
-  //       loadAlarmsOnPage(1);
-  //       addNotification(`Alarm removed`, 2000);
-  //     }
-  //   });
-  // }
 
   render() {
     const { alarm } = this.props;
