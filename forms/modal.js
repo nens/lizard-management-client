70ee9b87cbb1a517fd810d4eabfa7f622dc6553f
GeOdin <madeleine.vanwinkel@gmail.com>
2019-05-03 17:12:43 +0200

    Fix bug: modal can now be properly closed again, remove padding-10px class.

--- forms/modal.js
+++ forms/modal.js
@@ -1,15 +1,17 @@
 function openModal() {
-  // Get the modal
-  var modal = document.getElementById("about-roles-modal");
-  modal.style.display = "block";
+  // Get the modal page blocker that also contains the modal
+  var modal = document.getElementById("js-modal-page-blocker");
+  modal.classList.remove("not-shown");
 }
 
 function closeModal() {
-  // Get the modal
-  var modal = document.getElementById("about-roles-modal");
-  // Get the <span> element that closes the modal
-  var buttonModalClose = document.getElementById("modal-close-button");
-  if (event.target === modal || event.target === buttonModalClose) {
-    modal.style.display = "none";
+  // Get the modal page blocker that also contains the modal
+  var modal = document.getElementById("js-modal-page-blocker");
+  // Get the button that closes the modal page blocker that also contains the modal
+  var buttonModalClose = document.getElementById(
+    "js-close-modal-page-blocker-button"
+  );
+  if (event.target === buttonModalClose || event.target === modal) {
+    modal.classList.add("not-shown");
   }
 }
