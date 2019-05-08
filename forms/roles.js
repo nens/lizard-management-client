4468fee690c846ea7c0a0fa341fc9214a4e970ab
GeOdin <madeleine.vanwinkel@gmail.com>
2019-05-08 14:45:09 +0200

    Make the user roles in the user table editable.

--- forms/roles.js
+++ forms/roles.js
@@ -1,14 +1,29 @@
-function toggleEditRolesButton() {
-  console.log(event);
-  console.log(event.target);
-  if (
-    event.target.classList.contains("edit-role") &&
-    !event.target.classList.contains("button-shadow")
-  ) {
-    event.target.textContent = "SAVE";
-    event.target.classList.add("button-shadow");
-  } else {
-    event.target.textContent = "+";
-    event.target.classList.remove("button-shadow");
+function enableRoleCheckboxes() {
+  var userRoles = event.target.parentNode.children;
+  // Add interaction of the custom checkboxes
+  for (var i = 0; i < 4; i++) {
+    userRoles[i].childNodes[1].disabled = false;
+    userRoles[i].childNodes[3].classList.remove("disabled");
   }
+  // Remove the edit button and show the save button
+  event.target.classList.add("not-shown");
+  event.target.parentNode.children[5].classList.remove("not-shown");
+}
+
+function disableRoleCheckboxes() {
+  var userRoles = event.target.parentNode.children;
+  // Remove interaction of the custom checkboxes and
+  // add the not-checked class to the accompanying span if checkbox is clicked
+  for (var i = 0; i < 4; i++) {
+    if (userRoles[i].childNodes[1].checked === true) {
+      userRoles[i].childNodes[3].classList.remove("not-checked");
+    } else {
+      userRoles[i].childNodes[3].classList.add("not-checked");
+    }
+    userRoles[i].childNodes[1].disabled = true;
+    userRoles[i].childNodes[3].classList.add("disabled");
+  }
+  // Remove the save button and show the edit button
+  event.target.classList.add("not-shown");
+  event.target.parentNode.children[4].classList.remove("not-shown");
 }
