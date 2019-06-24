6fc6f79ddbba8365e19313a34e23d25da78fafc0
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-06-24 11:36:05 +0200

    add default value to slushbucket in constructor

--- src/forms/SlushBucket.tsx
+++ src/forms/SlushBucket.tsx
@@ -38,6 +38,10 @@ export default class SlushBucket extends Component<Props, State> {
       searchString: ""
     };
     this.handleKeyUp = this.handleKeyUp.bind(this);
+
+    if (this.props.value === undefined || this.props.value === null) {
+      props.valueChanged([])
+    }
   }
 
   handleKeyUp(e: any): void {
