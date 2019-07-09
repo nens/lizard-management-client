5acb7b0132c52dcce3ef2197287ee0204ddd3c39
hoanphungt <hoanphung.cpo@gmail.com>
2019-07-09 12:12:10 +0200

    Update the html, css and script files

--- user-management/script.js
+++ user-management/script.js
@@ -1,7 +1,42 @@
 function openDropdown() {
-    document.getElementById("user-profile_dropdown").classList.toggle("user-profile_dropdown-show");
+    const dropdown = document.getElementById("user-profile_dropdown");
+    if (dropdown.style.display === "flex") {
+        dropdown.style.display = "none"
+    } else {
+        dropdown.style.display = "flex";
+    }
+};
+//When user click outside of the modal, close it
+// window.onclick = function(event) {
+//     const dropdown = document.getElementById("user-profile_dropdown");
+//     const userProfile = document.getElementById("user-profile");
+
+//     console.log(event.target === userProfile)
+//     console.log(dropdown.style.display)
+
+//     if (event.target !== dropdown && event.target === userProfile) {
+//         if (dropdown.style.display === "none") {
+//             dropdown.style.display = "flex"
+//         } else {
+//             dropdown.style.display = "none"
+//         }
+//     } else if (event.target !== dropdown && event.target !== userProfile) {
+//         dropdown.style.display = "none"
+//     }
+// }
+
+//Organisation switcher
+const openOrganisationSwitcher = () => {
+    window.location.href = "#switcher";
 };
 
-const openOrganisationSwitcher = () => {
-    window.location.href = "#switcher"
-}
+const handleOnOrganisationClick = () => {
+    window.location.href = "#"
+};
+
+const handleInput = () => {
+    const x = document.getElementById("organisationName").value;
+    console.log(x)
+};
+
+
