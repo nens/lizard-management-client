f9951a29b7813195aebcfad4568479475e0037cb
hoanphungt <hoanphung.cpo@gmail.com>
2019-07-10 12:46:16 +0200

    Update header in the user-management-screen

--- user-management/script.js
+++ user-management/script.js
@@ -1,29 +1,16 @@
-function openDropdown() {
+//Open and close the user profile dropdown
+const openDropdown = () => {
     const dropdown = document.getElementById("user-profile_dropdown");
     if (dropdown.style.display === "flex") {
         dropdown.style.display = "none"
     } else {
-        dropdown.style.display = "flex";
+        dropdown.style.display = "flex"
     }
 };
-//When user click outside of the modal, close it
-// window.onclick = function(event) {
-//     const dropdown = document.getElementById("user-profile_dropdown");
-//     const userProfile = document.getElementById("user-profile");
-
-//     console.log(event.target === userProfile)
-//     console.log(dropdown.style.display)
-
-//     if (event.target !== dropdown && event.target === userProfile) {
-//         if (dropdown.style.display === "none") {
-//             dropdown.style.display = "flex"
-//         } else {
-//             dropdown.style.display = "none"
-//         }
-//     } else if (event.target !== dropdown && event.target !== userProfile) {
-//         dropdown.style.display = "none"
-//     }
-// }
+const closeDropdown = () => {
+    const dropdown = document.getElementById("user-profile_dropdown");
+    dropdown.style.display = "none"
+};
 
 //Organisation switcher
 const openOrganisationSwitcher = () => {
@@ -38,5 +25,3 @@ const handleInput = () => {
     const x = document.getElementById("organisationName").value;
     console.log(x)
 };
-
-
