dfb61632e991a0d17f7ea1115ad5206928ae578d
hoanphungt <hoanphung.cpo@gmail.com>
2019-08-07 14:17:56 +0200

    Add scenarios screen into data management screen

---
+++ src/data_management/scenarios/Scenarios.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Scenarios extends Component {
+    render() {
+        return <h1>3Di Scenariosssssssssssssssss</h1>
+    }
+}
+
+export { Scenarios };
