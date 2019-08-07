23f1bf263d454af449590753fa1e8e1fd3b80cca
hoanphungt <hoanphung.cpo@gmail.com>
2019-08-07 15:08:54 +0200

    API call for scenarios

--- src/data_management/scenarios/Scenarios.js
+++ src/data_management/scenarios/Scenarios.js
@@ -1,9 +1,37 @@
 import React, {Component} from 'react';
 
 class Scenarios extends Component {
+    state = {
+        isFetching: false,
+        scenarios: []
+    };
+
+    getScenariosFromApi = () => {
+        const url = "/api/v3/scenarios";
+
+        this.setState({
+            isFetching: true
+        });
+
+        fetch(url, {
+            credentials: "same-origin"
+        })
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                scenarios: data.results,
+                isFetching: false
+            });
+        });
+    };
+
+    componentDidMount() {
+        this.getScenariosFromApi();
+    };
+
     render() {
         return <h1>3Di Scenariosssssssssssssssss</h1>
-    }
-}
+    };
+};
 
 export { Scenarios };
