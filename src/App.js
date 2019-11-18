20c3eb6714177ae642cd05e0c44415883a0ee110
GeOdin <madeleine.vanwinkel@gmail.com>
2019-11-18 17:37:15 +0100

    Remove no longer needed this.uuidRegex in src/App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ class App extends Component {
       showOrganisationSwitcher: false,
       showProfileList: false
     };
-    this.uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
     this.updateOnlineStatus = this.updateOnlineStatus.bind(this);
     this.updateViewportDimensions = this.updateViewportDimensions.bind(this);
   }
