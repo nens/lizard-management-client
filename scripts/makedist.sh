748d5c94c587553b57bbf32e2c838091d13906df
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-10-07 13:34:40 +0200

    Fix makedist.sh

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -7,7 +7,7 @@ mkdir -p /tmp/dist
 cp -r $DIRECTORY/build/* /tmp/dist/
 cd /tmp/dist
 mkdir -p $DIRECTORY/dist
-zip -r $DIRECTORY/dist/hittestresstool-client.zip .
+zip -r $DIRECTORY/dist/lizard-management-client.zip .
 cd /
 rm -r /tmp/dist
-ls -l $DIRECTORY/dist/hittestresstool-client.zip
+ls -l $DIRECTORY/dist/lizard-management-client.zip
