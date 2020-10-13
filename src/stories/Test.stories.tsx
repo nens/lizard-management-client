f5c370a756384da55cc5a195b74dc5d76892155d
Hoan Phung <hoan.phung@nelen-schuurmans.nl>
2020-10-13 16:43:45 +0200

    Add storybook (#472)

---
+++ src/stories/Test.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Test from './../Test';
+
+export default {
+  component: Test,
+  title: 'Test'
+}
+
+export const world = () => <Test name="world" />;
+
+export const people = () => <Test name="people" />;
