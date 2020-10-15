6a0971a136ac288fda842a109cfd5ff03377f31b
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2020-10-15 17:48:36 +0200

    make table with grid

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import styles from './Table.module.css';
+
+
 
 interface TestProps {
-  name: string
+  name: string;
+  tableData: any [];
 }
 
-const Test: React.FC<TestProps> = ({name}) => {
+const Table: React.FC<TestProps> = ({name, tableData}) => {
   return (
-    <div>Hello, {name}!</div>
+    <div>
+      <span>Hello, {name}!</span>
+      <div  className={styles.Table}>
+        <div>
+          <span>Name</span>
+          <span>Code</span>
+          <span>temporal</span>
+          <span>Size</span>
+          <span>Actions</span>
+        </div>
+        <div>
+        {
+          tableData.map(tableRow=>{
+            return (
+              <>
+                <span>{tableRow.name}</span>
+                <span>{tableRow.supplier_code}</span>
+                <span>{tableRow.temporal === true? "Yes" : "No"}</span>
+                <span>{tableRow.size}</span>
+                <span>2.5gb</span>
+              </>
+            );
+          })
+        }
+        </div>
+      </div>
+    </div>
+
   )
 };
 
-export default Test;
+export default Table;
