3f86c97fdc5de30fde4d33375374ff6cf42a0a9d
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2020-10-29 17:07:17 +0100

    draft dropdown button

--- src/components/TableActionButtons.tsx
+++ src/components/TableActionButtons.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {useState} from 'react';
+import actionsIcon from "../images/table_actions_button_icon.svg";
+
 
 export interface Action {
   displayValue: string;
@@ -15,22 +18,35 @@ interface Props {
 }
 
 const TableActionButtons: React.FC<Props> = ({actions, tableRow,tableData,setTableData,triggerReloadWithCurrentPage, triggerReloadWithBasePage }) => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
+    <>
+    <label htmlFor={`actiondropdownrow${tableRow.uuid}`}>
+      {/* <button> */}
+        {/* <img src={actionsIcon}/> */}
+        text
+      {/* </button> */}
+    
+   
     <select
-      value={"actions"}
-      onChange={event=>{
-        const value = event.target.value;
-        const currentAction = actions.find(action => action.displayValue === value)
-        if (currentAction) {
-          currentAction.actionFunction(tableRow,tableData,setTableData,triggerReloadWithCurrentPage, triggerReloadWithBasePage);
-        }
-      }}
+      name={`actiondropdownrow${tableRow.uuid}`}
+      id={`actiondropdownrow${tableRow.uuid}`}
+      // value={"actions"}
+      // onChange={event=>{
+      //   const value = event.target.value;
+      //   const currentAction = actions.find(action => action.displayValue === value)
+      //   if (currentAction) {
+      //     currentAction.actionFunction(tableRow,tableData,setTableData,triggerReloadWithCurrentPage, triggerReloadWithBasePage);
+      //   }
+      // }}
     >
       <option value={"actions"}>Actions</option>
       {
         actions.map(action => <option value={action.displayValue}>{action.displayValue}</option>)
       }
     </select>
+    </label>
+    </>
   )
 };
 
