48f6eef66339680b86707a96906b4ad590e928de
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-11-20 16:06:00 +0100

    Update code with old codes

--- src/form/UploadRasterData.tsx
+++ src/form/UploadRasterData.tsx
@@ -15,15 +15,56 @@ export const UploadRasterData: React.FC<MyProps> = (props) => {
     name,
   } = props;
 
-  const [acceptedFiles, setAcceptedFiles] = useState<any[]>([]);
+  const [acceptedFiles, setAcceptedFiles] = useState<File[]>([]);
   console.log('acceptedFiles', acceptedFiles);
 
-  const onDrop = (files: any) => {
+  // check for valid date
+  // https://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
+  const isValidDateObj = (d: Date) => {
+    if (Object.prototype.toString.call(d) === "[object Date]") {
+      // it is a date
+      if (isNaN(d.getTime())) {
+        // d.valueOf() could also work
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const filesArrayContainsFile = (files: File[], file: File) => {
+    return files.some(oneFile => filesAreEqual(oneFile, file));
+  };
+
+  const filesAreEqual = (a: File, b: File) => {
+    return a.name === b.name && a.size === b.size;
+  };
+
+  const getFileClientsideRejectionReason = (file: File) => {
+    if (filesArrayContainsFile(acceptedFiles, file)) {
+      return "A file with this name and size was already selected";
+    } else if (
+      !file.name.toLowerCase().endsWith(".tif") ||
+      !file.name.toLowerCase().endsWith(".tiff") ||
+      !file.name.toLowerCase().endsWith(".geotiff")
+    ) {
+      return "Only .tif .tiff or .geotiff files are valid";
+    } else {
+      return "Reason not known";
+    };
+  };
+
+  const onDrop = (files: File[]) => {
     console.log('files', files);
     setAcceptedFiles(acceptedFiles.concat(files))
   };
 
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: ["image/tiff", ".geotiff"] // accepted file types
+  });
 
   return (
     <label
