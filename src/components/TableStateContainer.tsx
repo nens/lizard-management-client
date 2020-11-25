f1e5d7af2fef4ce668dc2c63c855b74ec7a46492
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-11-25 14:04:45 +0100

    Change text in placeholder of searchbar of table

--- src/components/TableStateContainer.tsx
+++ src/components/TableStateContainer.tsx
@@ -228,7 +228,7 @@ const TableStateContainerElement: React.FC<Props> = ({ gridTemplateColumns, colu
           }}
           onClear={()=>setNameContains("")}
           value={nameContains}
-          placeholder={"Type to search"}
+          placeholder={"Type to search for name"}
         />
 
         {
