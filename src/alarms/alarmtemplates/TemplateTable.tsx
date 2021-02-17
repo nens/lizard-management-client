2ab77cb468f0f4b7cee4904e6070f21f4c95ed33
hoanphungt <hoanphung.cpo@gmail.com>
2021-02-17 16:47:26 +0100

    Add template list table and remove unused components

---
+++ src/alarms/alarmtemplates/TemplateTable.tsx
@@ -0,0 +1,215 @@
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import TableStateContainer from '../../components/TableStateContainer';
+import TableActionButtons from '../../components/TableActionButtons';
+import tableStyles from "../../components/Table.module.css";
+import { ExplainSideColumn } from '../../components/ExplainSideColumn';
+import { ModalDeleteContent } from '../../components/ModalDeleteContent'
+import Modal from '../../components/Modal';
+import wmsIcon from "../../images/wms@3x.svg";
+
+export const TemplateTable: React.FC<any> = (props) =>  {
+  const [rowsToBeDeleted, setRowsToBeDeleted] = useState<any[]>([]);
+  const [rowToBeDeleted, setRowToBeDeleted] = useState<any | null>(null);
+  const [deleteFunction, setDeleteFunction] = useState<null | Function>(null);
+  const [busyDeleting, setBusyDeleting] = useState<boolean>(false);
+
+  const baseUrl = "/api/v3/messages/";
+  const navigationUrl = "/alarms/templates";
+
+  const fetchTemplatesWithOptions = (ids: string[], fetchOptions:any) => {
+    const url = "/api/v3/messages/";
+    const fetches = ids.map (id => {
+      return (fetch(url + id + "/", fetchOptions));
+    });
+    return Promise.all(fetches)
+  }
+
+  const deleteActions = (rows: any[], tableData:any, setTableData:any, triggerReloadWithCurrentPage:any, triggerReloadWithBasePage:any, setCheckboxes: any)=>{
+    setRowsToBeDeleted(rows);
+    const ids = rows.map(row=> row.id);
+    setDeleteFunction(()=>()=>{
+      setBusyDeleting(true);
+      const tableDataDeletedmarker = tableData.map((rowAllTables:any)=>{
+        if (ids.find(id => id === rowAllTables.id)) {
+          return {...rowAllTables, markAsDeleted: true}
+        } else{
+          return {...rowAllTables};
+        }
+      })
+      setTableData(tableDataDeletedmarker);
+      const opts = {
+        credentials: "same-origin",
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({})
+      };
+      return fetchTemplatesWithOptions(ids, opts)
+      .then((_result) => {
+        if (setCheckboxes) {
+          setCheckboxes([]);
+        }
+        triggerReloadWithCurrentPage();
+        return new Promise((resolve, _reject) => {
+          resolve();
+        });
+      })
+    });
+  }
+
+  const deleteAction = (row: any, updateTableRow:any, triggerReloadWithCurrentPage:any, triggerReloadWithBasePage:any)=>{
+    
+    setRowToBeDeleted(row);
+    setDeleteFunction(()=>()=>{
+      setBusyDeleting(true);
+      updateTableRow({...row, markAsDeleted: true});
+      const opts = {
+        credentials: "same-origin",
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({})
+      };
+      return fetchTemplatesWithOptions([row.id], opts)
+      .then((_result) => {
+        setBusyDeleting(false);
+        // TODO: do we need this callback or should we otherwise indicate that the record is deleted ?
+        triggerReloadWithCurrentPage();
+        return new Promise((resolve, _reject) => {
+            resolve();
+          });
+        })
+      
+    })
+  }
+
+  const columnDefinitions = [
+    {
+      titleRenderFunction: () => "Name",
+      renderFunction: (row: any) => 
+        <span
+          className={tableStyles.CellEllipsis}
+          title={row.name}
+        >
+          <NavLink to={`${navigationUrl}/${row.id}/`}>{row.name}</NavLink>
+        </span>,
+      orderingField: "name",
+    },
+    {
+      titleRenderFunction: () =>  "Type",
+      renderFunction: (row: any) => 
+        <span
+          className={tableStyles.CellEllipsis}
+          title={row.type}
+        >
+          {row.type.toUpperCase()}
+        </span>,
+      orderingField: "type",
+    },
+    {
+      titleRenderFunction: () =>  "",//"Actions",
+      renderFunction: (row: any, tableData:any, setTableData:any, triggerReloadWithCurrentPage:any, triggerReloadWithBasePage:any) => {
+        return (
+            <TableActionButtons
+              tableRow={row} 
+              tableData={tableData}
+              setTableData={setTableData} 
+              triggerReloadWithCurrentPage={triggerReloadWithCurrentPage} 
+              triggerReloadWithBasePage={triggerReloadWithBasePage}
+              actions={[
+                {
+                  displayValue: "Delete",
+                  actionFunction: deleteAction,
+                },
+              ]}
+            />
+        );
+      },
+      orderingField: null,
+    },
+  ];
+
+  const handleNewContactClick  = () => {
+    const { history } = props;
+    history.push(`${navigationUrl}/new`);
+  }
+
+  return (
+    <ExplainSideColumn
+      imgUrl={wmsIcon}
+      imgAltDescription={"Template icon"}
+      headerText={"Templates"}
+      explanationText={"Templates are used to create messages for your alarms. You can choose between an email or text message."} 
+      backUrl={"/alarms"}
+    >
+        <TableStateContainer 
+          gridTemplateColumns={"10% 70% 10% 10%"} 
+          columnDefinitions={columnDefinitions}
+          baseUrl={`${baseUrl}?`} 
+          checkBoxActions={[
+            {
+              displayValue: "Delete",
+              actionFunction: deleteActions,
+            }
+          ]}
+          newItemOnClick={handleNewContactClick}
+          filterOptions={[
+            {
+              value: 'name__icontains=',
+              label: 'Name'
+            }
+          ]}
+        />
+        { 
+        rowsToBeDeleted.length > 0?
+           <Modal
+           title={'Are you sure?'}
+           buttonConfirmName={'Delete'}
+           onClickButtonConfirm={() => {
+              deleteFunction && deleteFunction().then(()=>{
+              setRowsToBeDeleted([]);
+              setDeleteFunction(null);
+             });
+           }}
+           cancelAction={()=>{
+            setRowsToBeDeleted([]);
+            setDeleteFunction(null);
+          }}
+          disableButtons={busyDeleting}
+         >
+           
+           <p>Are you sure? You are deleting the following templates:</p>
+           {ModalDeleteContent(rowsToBeDeleted, busyDeleting, [{name: "name", width: 30}, {name: "type", width: 20}, {name: "id", width: 50}])}
+           
+         </Modal>
+        :
+          null
+        }
+
+        { 
+        rowToBeDeleted?
+           <Modal
+           title={'Are you sure?'}
+           buttonConfirmName={'Delete'}
+           onClickButtonConfirm={() => {
+             deleteFunction && deleteFunction().then(()=>{
+              setRowToBeDeleted(null);
+              setDeleteFunction(null);
+             });
+             
+           }}
+           cancelAction={()=>{
+             setRowToBeDeleted(null);
+             setDeleteFunction(null);
+           }}
+           disableButtons={busyDeleting}
+         >
+           <p>Are you sure? You are deleting the following template:</p>
+           {ModalDeleteContent([rowToBeDeleted], busyDeleting, [{name: "name", width: 30}, {name: "type", width: 20}, {name: "id", width: 50}])}
+
+         </Modal>
+        :
+          null
+        }
+     </ExplainSideColumn>
+  );
+}
