81b2190cc7c64b178edf088a2e7c0e5edc7ec5be
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-02-23 09:21:57 +0100

    guard selected organisation

--- src/App.js
+++ src/App.js
@@ -182,7 +182,7 @@ class App extends Component {
     const currentHomeAppTile = appTiles.find(tile => {
       return window.location.href.includes(tile.linksTo)
     });
-    if (currentHomeAppTile && !doArraysHaveEqualElement(this.props.selectedOrganisation.roles, currentHomeAppTile.requiresOneOfRoles)) {
+    if (currentHomeAppTile && !doArraysHaveEqualElement(((this.props.selectedOrganisation && this.props.selectedOrganisation.roles) || []), currentHomeAppTile.requiresOneOfRoles)) {
       const redirectMessage = this.props.intl.formatMessage({ id: "authorization.redirected_based_onrole", defaultMessage: "You do not have the rights to access this data under the selected organisation. \nYou will be redirected." });
       alert(redirectMessage);
       // should redirect to <customer_url>.lizard.net/management/ on prod
