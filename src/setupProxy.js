d85ae6ab88e6bc3ec6fc6402e89200b7965e8262
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-02-23 10:27:35 +0100

    adapt proxy for no username password

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,18 +4,18 @@ module.exports = function(app) {
   const password = process.env.PROXY_PASSWORD;
   const username = process.env.PROXY_USERNAME;
 
-  if (!password || !username) {
-    console.log("Please set PROXY_PASSWORD and PROXY_USERNAME variables!");
-    process.exit(1);
-  }
-
   const proxyMiddleware = createProxyMiddleware({
     target: 'https://nxt3.staging.lizard.net/',
     // target: 'https://demo.lizard.net/',
     changeOrigin: true,
-    // Use HTTP basic auth, works with Lizard only
-    auth: `${username}:${password}`
   });
+
+  if (!password || !username) {
+    console.log("Proceeding without username password !");
+  } else {
+    // Use HTTP basic auth, works with Lizard only
+    proxyMiddleware.auth = `${username}:${password}`;
+  }
 
   app.use('/api', proxyMiddleware);
   app.use('/bootstrap', proxyMiddleware);
