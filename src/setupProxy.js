77631090a6a0890b866a4106eb20e1d8cadab283
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-02-23 15:17:13 +0100

    fix bug redirect and proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,17 +4,23 @@ module.exports = function(app) {
   const password = process.env.PROXY_PASSWORD;
   const username = process.env.PROXY_USERNAME;
 
-  const proxyMiddleware = createProxyMiddleware({
-    target: 'https://nxt3.staging.lizard.net/',
-    // target: 'https://demo.lizard.net/',
-    changeOrigin: true,
-  });
+  const stagingUrl = 'https://nxt3.staging.lizard.net/';
+  // const prodUrl = 'https://demo.lizard.net/';
 
+  let proxyMiddleware;
   if (!password || !username) {
     console.log("Proceeding without username password !");
+    proxyMiddleware = createProxyMiddleware({
+      target: stagingUrl,
+      changeOrigin: true,
+    });
   } else {
     // Use HTTP basic auth, works with Lizard only
-    proxyMiddleware.auth = `${username}:${password}`;
+    proxyMiddleware = createProxyMiddleware({
+      target: stagingUrl,
+      changeOrigin: true,
+      auth: `${username}:${password}`,
+    });
   }
 
   app.use('/api', proxyMiddleware);
