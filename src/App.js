b199d6ae25cc918d652a1893a2f663f503b370d2
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-02-26 13:22:02 +0100

    find closest tile

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ import helpIcon from './images/help.svg'
 import documentIcon from './images/document.svg';
 import logoutIcon from './images/logout.svg';
 import editIcon from './images/edit.svg';
+import arrayAmountOfIdenticallyOrderedItemsFromStart from './utils/arrayAmountOfIdenticallyOrderedItemsFromStart';
 
 class App extends Component {
   constructor(props) {
@@ -179,22 +180,48 @@ class App extends Component {
       window.location = "/";
     }
 
-    const currentHomeAppTile = appTiles.find(tile => {
-      return window.location.href.split("#")[1] === tile.linksTo
+    const urlSuffix = window.location.href.split("#")[1]
+    // currentHomeAppTile is used to decide if the user has authorisation to be here or should be redirected
+    // The tile you are currently on (have previously clicked on). Note: the user does not see this tile currently, but sees the screen it lead to.
+    // For example if the user clicks on the tile that has 'linksTo: "/data_management/scenarios",' it will see the scenarion list, 
+    // but currentHomeAppTile is then actually the tile with 'linksTo: "/data_management/scenarios",'
+    let currentHomeAppTile = appTiles.find(tile => {
+      return urlSuffix === tile.linksTo
     });
+    console.log("currentHomeAppTile", currentHomeAppTile,  this.props.selectedOrganisation && this.props.selectedOrganisation.roles);
+    // if currentHomeAppTile is not found the user is on a screen further away removed from the tiles. For example on the screen: /data_management/scenarios/{uuid}
+    // In that case the authorization from the closest by tile still applies. We need to find out which tile is closest by in a iteration
+    if (!currentHomeAppTile) {
+      appTiles.forEach(appTile=>{
+        if (!currentHomeAppTile) {
+          currentHomeAppTile = appTile;
+        } else {
+          const suffixItems = urlSuffix.split('/');
+          const loopTileItems = appTile.linksTo.split('/');
+          const existingTileItems = currentHomeAppTile.linksTo.split('/');
+          if (arrayAmountOfIdenticallyOrderedItemsFromStart(suffixItems,loopTileItems) > arrayAmountOfIdenticallyOrderedItemsFromStart(suffixItems,existingTileItems)) {
+            currentHomeAppTile = appTile
+          }
+        } 
+      })
+    }
+    console.log("currentHomeAppTile2", currentHomeAppTile.linksTo, currentHomeAppTile,  this.props.selectedOrganisation && this.props.selectedOrganisation.roles);
+
+
     if (
       // only link back if it is not already the home page tile
       ((currentHomeAppTile && currentHomeAppTile.linksTo !== "/") || !currentHomeAppTile)  &&
       // only link back if the user is actually already autheniticated
       this.props.bootstrap && this.props.bootstrap.isAuthenticated &&
-      !doArraysHaveEqualElement(((this.props.selectedOrganisation && this.props.selectedOrganisation.roles) || []), currentHomeAppTile.requiresOneOfRoles)
+      this.props.selectedOrganisation &&
+      !doArraysHaveEqualElement(((this.props.selectedOrganisation.roles) || []), (currentHomeAppTile && currentHomeAppTile.requiresOneOfRoles) || ["user", "manager", "supplier", "admin"])
       ) {
       const redirectMessage = this.props.intl.formatMessage({ id: "authorization.redirected_based_onrole", defaultMessage: "You do not have the rights to access this data under the selected organisation. \nYou will be redirected." });
       alert(redirectMessage);
       // should redirect to <customer_url>.lizard.net/management/ on prod
       this.props.history.push("/");
     }
-    if (!this.props.isAuthenticated) {
+    if (!this.props.isAuthenticated || !this.props.selectedOrganisation) {
       return (
         <div className={styles.MDSpinner}>
           <MDSpinner size={24} />
---
+++ src/utils/arrayAmountOfIdenticallyOrderedItemsFromStart.js
@@ -0,0 +1,16 @@
+const arrayAmountOfIdenticallyOrderedItemsFromStart = function (arr1, arr2) {
+  let i = 0;
+  let equalItems = 0;
+  let foundDifference = false
+  while (!foundDifference && arr1.length > i) {
+    i++;
+    if (arr1[i] === arr2[i]) {
+      equalItems++;
+    } else {
+      foundDifference = true;
+    }
+  }
+  return equalItems;
+}
+
+export default arrayAmountOfIdenticallyOrderedItemsFromStart;
