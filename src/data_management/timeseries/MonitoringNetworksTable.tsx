b1e05ccfe73294d9754fc1eaea6affef8b03d09a
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-03-24 09:53:41 +0100

    draft tiles and timeseries table

---
+++ src/data_management/timeseries/MonitoringNetworksTable.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import TableStateContainer from '../../components/TableStateContainer';
+import { NavLink } from "react-router-dom";
+import {ExplainSideColumn} from '../../components/ExplainSideColumn';
+import tableStyles from "../../components/Table.module.css";
+import timeseriesIcon from "../../images/timeseries_icon.svg";
+
+
+const baseUrl = "/api/v4/timeseries/";
+const navigationUrl = "/data_management/timeseries/timeseries";
+
+
+
+const columnDefinitions = [
+  {
+    titleRenderFunction: () => "Name",
+    renderFunction: (row: any) => 
+      <span
+        className={tableStyles.CellEllipsis}
+        title={row.name}
+      >
+        <NavLink to={`${navigationUrl}/${row.uuid}`}>{!row.name? "(empty name)" : row.name }</NavLink>
+      </span>
+    ,
+    orderingField: "name",
+  },
+  {
+    titleRenderFunction: () =>  "Uuid",
+    renderFunction: (row: any) =>
+      <span
+        className={tableStyles.CellEllipsis}
+        title={row.uuid}
+      >
+        {row.uuid}
+      </span>
+    ,
+    orderingField: null,
+  },
+];
+
+export const MonitoringNetworksTable = (props:any) =>  {
+
+  return (
+    <ExplainSideColumn
+      imgUrl={timeseriesIcon}
+      imgAltDescription={"Timeseries icon"}
+      headerText={"Timeseries"}
+      explanationText={"Search or sort your timeseries here."}
+      backUrl={"/data_management/timeseries"}
+    >
+      <TableStateContainer 
+        gridTemplateColumns={"60% 40%"} 
+        columnDefinitions={columnDefinitions}
+        baseUrl={`${baseUrl}?`} 
+        checkBoxActions={[]}
+        filterOptions={[
+          {value: 'name__icontains=', label: 'Name'},
+          {value: 'uuid=', label: 'UUID'},
+        ]}
+      />
+    </ExplainSideColumn>
+  );
+}
