2f3e58b84e23f7015bd6456fc8b960512fec6acf
Tom de Boer <Derryrover@users.noreply.github.com>
2021-03-24 13:02:06 +0100

    Revert "timeseries table (#734)"
    This reverts commit b9bd2703f241975eb9e65c2de39d37555302ef4d.

--- src/data_management/timeseries/TimeseriesTable.tsx
+++ src/data_management/timeseries/TimeseriesTable.tsx
@@ -5,8 +5,6 @@ import { NavLink } from "react-router-dom";
 import {ExplainSideColumn} from '../../components/ExplainSideColumn';
 import tableStyles from "../../components/Table.module.css";
 import timeseriesIcon from "../../images/timeseries_icon.svg";
-import TableActionButtons from '../../components/TableActionButtons';
-
 
 
 const baseUrl = "/api/v4/timeseries/";
@@ -20,7 +18,7 @@ const columnDefinitions = [
     renderFunction: (row: any) => 
       <span
         className={tableStyles.CellEllipsis}
-        title={`${row.name} - UUID: ${row.uuid}`}
+        title={row.name}
       >
         <NavLink to={`${navigationUrl}/${row.uuid}`}>{!row.name? "(empty name)" : row.name }</NavLink>
       </span>
@@ -28,110 +26,15 @@ const columnDefinitions = [
     orderingField: "name",
   },
   {
-    titleRenderFunction: () => "Observation type",
-    renderFunction: (row: any) => 
+    titleRenderFunction: () =>  "Uuid",
+    renderFunction: (row: any) =>
       <span
         className={tableStyles.CellEllipsis}
+        title={row.uuid}
       >
-        {!row.observation_type? "(empty observation type)" : 
-          row.observation_type.unit? <>{row.observation_type.code}{" "} <span style={{fontWeight:600,}}>{`(${row.observation_type.unit})`}</span></> : 
-          row.observation_type.code 
-        }
+        {row.uuid}
       </span>
     ,
-    orderingField: "observation_type",
-  },
-  {
-    titleRenderFunction: () => "Location",
-    renderFunction: (row: any) => 
-      <span
-        className={tableStyles.CellEllipsis}
-        style={{
-          // overflowX: "hidden",
-          // display: "flex",
-          // overwrite default behaviour tableStyles.CellEllipsis 
-          whiteSpace: "break-spaces",
-        }}
-        title={`name: ${row.location.name}, code: ${row.location.code}`}
-      >
-        {
-        !row.location? "(empty location)" : 
-        row.location.code && row.location.code !==  row.location.name? 
-        <> 
-          <span
-            // className={tableStyles.CellEllipsis}
-            // style={{
-            //   // display: "flex", 
-            //   // flexBasis: "content", 
-            //   // flexShrink: 0,
-            //   minWidth: "10px",
-            // }}
-          >
-            {row.location.name}
-          </span>
-          <span
-            // className={tableStyles.CellEllipsis}
-            // style={{
-            //   display: "flex", 
-            //   flexBasis: "content", 
-            //   flexShrink: 0,
-            // }}
-          >
-          {` (${row.location.code})`} 
-          </span>
-        </> 
-        :
-        row.location.name
-        }
-      </span>
-    ,
-    orderingField: "location",
-  },
-  {
-    titleRenderFunction: () => "Accessibility",
-    renderFunction: (row: any) => 
-      <span
-        className={tableStyles.CellEllipsis}
-        title={row.access_modifier}
-      >
-        {row.access_modifier }
-      </span>
-    ,
-    orderingField: "access_modifier",
-  },
-  // // uuid for now not needed
-  // {
-  //   titleRenderFunction: () =>  "Uuid",
-  //   renderFunction: (row: any) =>
-  //     <span
-  //       className={tableStyles.CellEllipsis}
-  //       title={row.uuid}
-  //     >
-  //       {row.uuid}
-  //     </span>
-  //   ,
-  //   orderingField: null,
-  // },
-  {
-    titleRenderFunction: () =>  "",//"Actions",
-    renderFunction: (row: any, tableData:any, setTableData:any, triggerReloadWithCurrentPage:any, triggerReloadWithBasePage:any) => {
-      return (
-          <TableActionButtons
-            tableRow={row} 
-            tableData={tableData}
-            setTableData={setTableData} 
-            triggerReloadWithCurrentPage={triggerReloadWithCurrentPage} 
-            triggerReloadWithBasePage={triggerReloadWithBasePage}
-            editUrl={`${navigationUrl}/${row.uuid}`}
-            actions={[
-              // {
-              //   displayValue: "Delete",
-              //   actionFunction: deleteActionRaster,
-              // },
-            ]}
-          />
-      );
-    },
     orderingField: null,
   },
 ];
@@ -147,12 +50,12 @@ export const TimeseriesTable = (props:any) =>  {
       backUrl={"/data_management/timeseries"}
     >
       <TableStateContainer 
-        gridTemplateColumns={"24% 22% 24% 22% 8%"} 
+        gridTemplateColumns={"60% 40%"} 
         columnDefinitions={columnDefinitions}
         baseUrl={`${baseUrl}?`} 
         checkBoxActions={[]}
         filterOptions={[
-          {value: 'name__startswith=', label: 'Name'},
+          {value: 'name__icontains=', label: 'Name'},
           {value: 'uuid=', label: 'UUID'},
         ]}
       />
