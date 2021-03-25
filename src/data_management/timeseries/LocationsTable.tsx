035a232b892f4b54b1247423ffa6166328e65e7b
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-03-25 14:14:08 +0100

    location table

--- src/data_management/timeseries/LocationsTable.tsx
+++ src/data_management/timeseries/LocationsTable.tsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 import {ExplainSideColumn} from '../../components/ExplainSideColumn';
 import tableStyles from "../../components/Table.module.css";
 import locationIcon from "../../images/locations_icon.svg";
-
+import TableActionButtons from '../../components/TableActionButtons';
 
 const baseUrl = "/api/v4/locations/";
 const navigationUrl = "/data_management/timeseries/locations";
@@ -26,20 +26,71 @@ const columnDefinitions = [
     orderingField: "name",
   },
   {
-    titleRenderFunction: () =>  "Uuid",
-    renderFunction: (row: any) =>
+    titleRenderFunction: () => "Code",
+    renderFunction: (row: any) => 
       <span
         className={tableStyles.CellEllipsis}
-        title={row.uuid}
+        title={row.code}
       >
-        {row.uuid}
+        {row.code}
       </span>
     ,
+    orderingField: "code",
+  },
+  {
+    titleRenderFunction: () => "Accessibility",
+    renderFunction: (row: any) => 
+      <span
+        className={tableStyles.CellEllipsis}
+        title={row.access_modifier}
+      >
+        {row.access_modifier }
+      </span>
+    ,
+    orderingField: "access_modifier",
+  },
+  // {
+  //   titleRenderFunction: () =>  "Uuid",
+  //   renderFunction: (row: any) =>
+  //     <span
+  //       className={tableStyles.CellEllipsis}
+  //       title={row.uuid}
+  //     >
+  //       {row.uuid}
+  //     </span>
+  //   ,
+  //   orderingField: null,
+  // },
+  {
+    titleRenderFunction: () =>  "",//"Actions",
+    renderFunction: (row: any, tableData:any, setTableData:any, triggerReloadWithCurrentPage:any, triggerReloadWithBasePage:any) => {
+      return (
+          <TableActionButtons
+            tableRow={row} 
+            tableData={tableData}
+            setTableData={setTableData} 
+            triggerReloadWithCurrentPage={triggerReloadWithCurrentPage} 
+            triggerReloadWithBasePage={triggerReloadWithBasePage}
+            editUrl={`${navigationUrl}/${row.uuid}`}
+            actions={[
+              // {
+              //   displayValue: "Delete",
+              //   actionFunction: deleteActionRaster,
+              // },
+            ]}
+          />
+      );
+    },
     orderingField: null,
   },
 ];
 
 export const LocationsTable = (props:any) =>  {
+
+  const handleNewClick  = () => {
+    const { history } = props;
+    history.push(`${navigationUrl}/new`);
+  }
 
   return (
     <ExplainSideColumn
@@ -50,12 +101,13 @@ export const LocationsTable = (props:any) =>  {
       backUrl={"/data_management/locations"}
     >
       <TableStateContainer 
-        gridTemplateColumns={"60% 40%"} 
+        gridTemplateColumns={"40% 40% 12% 8%"} 
         columnDefinitions={columnDefinitions}
         baseUrl={`${baseUrl}?`} 
         checkBoxActions={[]}
+        newItemOnClick={handleNewClick}
         filterOptions={[
-          {value: 'name__icontains=', label: 'Name'},
+          {value: 'name__startswith=', label: 'Name'},
           {value: 'uuid=', label: 'UUID'},
         ]}
       />
