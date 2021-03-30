65b761a52e204b92c1d7cf7658e1af22b35934a4
hoanphungt <hoanphung.cpo@gmail.com>
2021-03-30 11:12:37 +0200

    Fix attempt 1

--- src/types/timeseriesType.ts
+++ src/types/timeseriesType.ts
@@ -1,4 +1,31 @@
 export interface Timeseries {
   uuid: string,
-  name: string
+  name: string,
+  parameter: string,
+  unit: string,
+  reference_frame: string,
+  observation_type?: {
+    code: string,
+    parameter: string,
+    unit: string,
+    reference_frame: string
+  }
+}
+
+export const getTimeseriesLabel = (ts: Timeseries) => {
+  if (ts.observation_type && ts.observation_type.parameter) {
+    if (ts.observation_type.unit) {
+      return `${ts.observation_type.parameter} (${ts.observation_type.unit})`;
+    } else {
+      return ts.observation_type.parameter;
+    };
+  } else if (ts.parameter) {
+    if (ts.unit) {
+      return `${ts.parameter} (${ts.unit})`;
+    } else {
+      return ts.parameter;
+    };
+  } else {
+    return ts.name;
+  };
 }
