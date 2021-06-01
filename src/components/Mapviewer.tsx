86106bfcc29d166cee8baa38efa414c84948476c
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-06-01 10:28:10 +0200

    make map fullscreen

--- src/components/Mapviewer.tsx
+++ src/components/Mapviewer.tsx
@@ -27,8 +27,13 @@ function MapViewer (props: MyProps & DispatchProps) {
   return (
     <div 
       style={{
-        width:"100%",
-        height:"100%",
+        // width:"100%",
+        // height:"100%",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width:"100vw",
+        height:"100vh",
       }}
     >
       <ReactMapGL
