1bc01f0491b7ab0c00e8ab83e1115cc336d72d42
hoanphungt <hoanphung.cpo@gmail.com>
2021-07-02 15:21:00 +0200

    Improve fetch hook

--- src/utils/usePaginatedFetch.ts
+++ src/utils/usePaginatedFetch.ts
@@ -6,21 +6,26 @@ interface InputProps {
 
 interface Output {
   fetchingState: string | null;
-  results: any[];
+  results: any[] | null;
   count: number;
 }
 
 export const usePaginatedFetch = (props: InputProps): Output => {
   const { url } = props;
-  const [nextUrl, setNextUrl] = useState<string | null>(null);
-  const [results, setResults] = useState<any[]>([]);
+  const [results, setResults] = useState<any[] | null>(null);
   const [count, setCount] = useState<number>(0);
   const [fetchingState, setFetchingState] = useState<string | null>(null);
+
+  const resetAllState = () => {
+    setResults(null);
+    setFetchingState(null);
+    setCount(0);
+  };
 
   useEffect(() => {
     // Recursive function to fetch next URLs
     const fetchHelper = async (url: string | null) => {
-      if (!url) return;
+      if (!url) return; // do nothing
 
       setFetchingState('FETCHING');
       try {
@@ -31,9 +36,9 @@ export const usePaginatedFetch = (props: InputProps): Output => {
         if (response.status === 200) {
           const data = await response.json();
           setCount(data.count);
-          setResults(results => results.concat(data.results));
+          setResults(results => results ? results.concat(data.results) : data.results);
           if (data.next) {
-            setNextUrl(data.next.split("lizard.net")[1]);
+            fetchHelper(data.next.split("lizard.net")[1]);
           } else {
             // Finish building data based on the "next" param
             setFetchingState('DONE');
@@ -49,8 +54,9 @@ export const usePaginatedFetch = (props: InputProps): Output => {
       };
     };
 
-    fetchHelper(nextUrl || url);
-  }, [url, nextUrl]);
+    fetchHelper(url);
+    return () => resetAllState();
+  }, [url]);
 
   return {
     fetchingState: fetchingState,
