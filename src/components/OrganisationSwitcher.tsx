54e774bd6a6a15f21133faead730e9214bd1bc46
Tom de Boer <Derryrover@users.noreply.github.com>
2021-08-19 16:24:40 +0200

    remove switched organisation snackbar (#930)

--- src/components/OrganisationSwitcher.tsx
+++ src/components/OrganisationSwitcher.tsx
@@ -56,127 +56,123 @@ const OrganisationSwitcher = (props:Props) => {
     setFilterValue( e.target.value);
   }
   
-  // todo fix this any
-  const selectOrganisation = (organisation:any) => {
-    props.selectOrganisation(organisation, true);
-  }
-    const {
-      handleClose,      
-    } = props;
+  const {
+    handleClose,      
+  } = props;
 
-    const selectedOrganisation = useSelector(getSelectedOrganisation);
-    const reduxOrganisations = useSelector(getOrganisations);
-    const isFetching = reduxOrganisations.isFetching;
-    const organisations = reduxOrganisations.available;
+  const selectedOrganisation = useSelector(getSelectedOrganisation);
+  const reduxOrganisations = useSelector(getOrganisations);
+  const isFetching = reduxOrganisations.isFetching;
+  const organisations = reduxOrganisations.available;
 
-    const filteredOrganisations = filterValue
-    // todo: fix this any
-      ? organisations.filter((org:any) => {
-        if (org.name.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1) {
-            return org;
-          }
-          return false;
-        })
-      : organisations;
+  const filteredOrganisations = filterValue
+  // todo: fix this any
+    ? organisations.filter((org:any) => {
+      if (org.name.toLowerCase().indexOf(filterValue.toLowerCase()) !== -1) {
+          return org;
+        }
+        return false;
+      })
+    : organisations;
 
-    // todo add type defenitions for props.intl.formatMessage
-    // @ts-ignore
-    const authorisationText = props.intl.formatMessage({ id: "authorization.organisation_not_allowed_current_page", defaultMessage: "! Organisation not authorized to visit current page !" });
+  // todo add type defenitions for props.intl.formatMessage
+  // @ts-ignore
+  const authorisationText = props.intl.formatMessage({ id: "authorization.organisation_not_allowed_current_page", defaultMessage: "! Organisation not authorized to visit current page !" });
 
-        
+      
 
-    return (
-      <div className={styles.OrganisationSwitcherContainer}>
-        <CSSTransition
-          in={true}
-          appear={true}
-          timeout={500}
-          classNames={{
-            enter: styles.Enter,
-            enterActive: styles.EnterActive,
-            // Todo find out how to solve this
-            // @ts-ignore
-            leave: styles.Leave,
-            leaveActive: styles.LeaveActive,
-            appear: styles.Appear,
-            appearActive: styles.AppearActive
-          }}
-        >
-          <div className={styles.OrganisationSwitcher}>
-            <div className={styles.CloseButton} onClick={handleClose}>
-              <i className="material-icons">close</i>
-            </div>
-            <h3>
-              <FormattedMessage
-                id="components.switch_org"
-                defaultMessage="Switch organisation"
-              />
-            </h3>
-            <br />
-            <div className={formStyles.FormGroup}>
-              <input
-                id="organisationName"
-                tabIndex={-1}
-                type="text"
-                className={formStyles.FormControl}
-                placeholder="Type here to filter the list of organisations..."
-                onChange={handleInput}
-              />
-            </div>
-            {isFetching ? (
-              <div
-                style={{
-                  position: "relative",
-                  top: 50,
-                  height: 300,
-                  bottom: 50,
-                  marginLeft: "50%"
-                }}
-              >
-                <MDSpinner size={24} />
-              </div>
-            ) : (
-              <Scrollbars
-                style={{ width: "100%", height: height - 400 }}
-              >
-                {filteredOrganisations
-                // Todo fix any og organisation
-                  ? filteredOrganisations.map((organisation:any, i:number) => {
-                      const hasRequiredRoles = userHasCorrectRolesForCurrentNavigationLinkTile(organisation.roles);
-                      return (
-                        <div
-                          key={organisation.uuid}
-                          className={`${styles.OrganisationRow} ${selectedOrganisation &&
-                          organisation.uuid === selectedOrganisation.uuid
-                            ? styles.Active
-                            : styles.InActive}`}
-                          onClick={() => {
-                            selectOrganisation(organisation);
-                            handleClose();
-                          }}
-                        >
-                          <i className="material-icons">group</i>
-                          <div className={styles.OrganisationName}>
-                            {organisation.name}
-                          </div>
-                          <div className={styles.OrganisationAuthorised}>
-                          {!hasRequiredRoles? authorisationText  : null}
-                          </div>
-                        </div>
-                      );
-                    })
-                  : null}
-              </Scrollbars>
-            )}
+  return (
+    <div className={styles.OrganisationSwitcherContainer}>
+      <CSSTransition
+        in={true}
+        appear={true}
+        timeout={500}
+        classNames={{
+          enter: styles.Enter,
+          enterActive: styles.EnterActive,
+          // Todo find out how to solve this
+          // @ts-ignore
+          leave: styles.Leave,
+          leaveActive: styles.LeaveActive,
+          appear: styles.Appear,
+          appearActive: styles.AppearActive
+        }}
+      >
+        <div className={styles.OrganisationSwitcher}>
+          <div className={styles.CloseButton} onClick={handleClose}>
+            <i className="material-icons">close</i>
           </div>
-        </CSSTransition>
-      </div>
-    );
-  }
+          <h3>
+            <FormattedMessage
+              id="components.switch_org"
+              defaultMessage="Switch organisation"
+            />
+          </h3>
+          <br />
+          <div className={formStyles.FormGroup}>
+            <input
+              id="organisationName"
+              tabIndex={-1}
+              type="text"
+              className={formStyles.FormControl}
+              placeholder="Type here to filter the list of organisations..."
+              onChange={handleInput}
+            />
+          </div>
+          {isFetching ? (
+            <div
+              style={{
+                position: "relative",
+                top: 50,
+                height: 300,
+                bottom: 50,
+                marginLeft: "50%"
+              }}
+            >
+              <MDSpinner size={24} />
+            </div>
+          ) : (
+            <Scrollbars
+              style={{ width: "100%", height: height - 400 }}
+            >
+              {filteredOrganisations
+              // Todo fix any og organisation
+                ? filteredOrganisations.map((organisation:any, i:number) => {
+                    const hasRequiredRoles = userHasCorrectRolesForCurrentNavigationLinkTile(organisation.roles);
+                    return (
+                      <div
+                        key={organisation.uuid}
+                        className={`${styles.OrganisationRow} ${selectedOrganisation &&
+                        organisation.uuid === selectedOrganisation.uuid
+                          ? styles.Active
+                          : styles.InActive}`}
+                        onClick={() => {
+                          props.selectOrganisation(organisation);
+                          handleClose();
+                        }}
+                      >
+                        <i className="material-icons">group</i>
+                        <div className={styles.OrganisationName}>
+                          {organisation.name}
+                        </div>
+                        <div className={styles.OrganisationAuthorised}>
+                        {!hasRequiredRoles? authorisationText  : null}
+                        </div>
+                      </div>
+                    );
+                  })
+                : null}
+            </Scrollbars>
+          )}
+        </div>
+      </CSSTransition>
+    </div>
+  );
+}
 
 const mapDispatchToProps = (dispatch:any) => {
   return {
-    selectOrganisation: (organisation:any, addNotification: boolean) => dispatch(selectOrganisation(organisation, addNotification))
+    selectOrganisation: (organisation:any) => dispatch(selectOrganisation(organisation))
   };
 };
 
