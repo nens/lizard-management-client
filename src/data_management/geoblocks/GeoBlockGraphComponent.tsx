3e8dcad5d2091e8f21b7b62c87a7aa66ee533c9d
hoanphungt <hoanphung.cpo@gmail.com>
2021-09-08 10:49:03 +0200

    Add geoblock graph component

---
+++ src/data_management/geoblocks/GeoBlockGraphComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Rete from 'rete';
+import ConnectionPlugin from 'rete-connection-plugin';
+import ReactRenderPlugin from 'rete-react-render-plugin';
+
+const numSocket = new Rete.Socket('Number value');
+
+
+
+export const GeoBlockGraphComponent = () => {
+  return (
+    <div></div>
+  )
+}
