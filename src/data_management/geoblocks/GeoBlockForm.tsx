d076dd1c6273aa3b50efa63c5992bbf13b46b943
hoanphungt <hoanphung.cpo@gmail.com>
2021-09-30 16:53:53 +0200

    Remove console.log

--- src/data_management/geoblocks/GeoBlockForm.tsx
+++ src/data_management/geoblocks/GeoBlockForm.tsx
@@ -141,7 +141,6 @@ const GeoBlockForm: React.FC<Props & DispatchProps & RouteComponentProps> = (pro
     handleReset,
     clearInput,
   } = useForm({initialValues, onSubmit});
-  console.log(values.source)
 
   // Delete modal
   const [showDeleteModal, setShowDeleteModal] = useState(false);
