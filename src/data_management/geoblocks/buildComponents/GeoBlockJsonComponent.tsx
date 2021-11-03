bc360d7e7157883003551ed914f31aaf2810818b
hoanphungt <hoanphung.cpo@gmail.com>
2021-11-03 20:58:53 +0100

    Use ace editor for json editor component

--- src/data_management/geoblocks/buildComponents/GeoBlockJsonComponent.tsx
+++ src/data_management/geoblocks/buildComponents/GeoBlockJsonComponent.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import ace from 'brace';
 import { GeoBlockSource } from '../../../types/geoBlockType';
 import { JsonEditor } from 'jsoneditor-react';
 import 'jsoneditor-react/es/editor.min.css';
+import 'brace/mode/json';
 
 interface MyProps {
   source: GeoBlockSource | null,
@@ -16,6 +18,7 @@ export const GeoBlockJsonComponent = (props: MyProps) => {
       value={source ? source : {}}
       onChange={e => setSource(e)}
       allowedModes={['tree', 'code', 'text']}
+      ace={ace}
       htmlElementProps={{style: {
         position: 'absolute',
         width: '100%',
