24f94a869379af3c5537f3d2b18611530a68cda3
hoanphungt <hoanphung.cpo@gmail.com>
2021-11-07 22:13:26 +0100

    Update help text for geoblock form

--- src/utils/help_texts/helpTextForGeoBlock.tsx
+++ src/utils/help_texts/helpTextForGeoBlock.tsx
@@ -1,9 +1,13 @@
-// import React from 'react';
+import React from 'react';
 import {
   accessModifierHelpText,
   descriptionHelpText,
   HelpText,
+  layercollectionHelpText,
   nameHelpText,
+  organisationHelpText,
+  organisationsToSharedWithHelpText,
+  sharedWithCheckboxHelpText,
   supplierHelpText,
   uuidHelpText
 } from './defaultHelpText';
@@ -13,8 +17,22 @@ export const geoBlockHelpText: HelpText = {
   name: nameHelpText,
   uuid: uuidHelpText,
   description: descriptionHelpText,
-  rasterSourceModal: 'Open modal to view connected raster source(s) of this layer.',
+  layercollections: layercollectionHelpText,
+  aggregationType: (
+    <>
+      <p>Specify how data should be aggregated in the Lizard portal.</p>
+      <p><i>This variable is only used in the "region selection" of the Lizard portal.</i></p>
+    </>
+  ),
   observationType: 'Choose the way the data is measured and the units.',
+  colorMap: 'Choose a color range that is displayed on the Lizard portal.',
+  colormap_minimum: 'Lowest value in the colorscale',
+  colormap_maximum: 'Highest value in the colorscale',
+  colorMap_rescalable: 'Make color map rescalable.',
+  geoBlockBuildModal: 'Open modal to build this GeoBlock.',
   accessModifier: accessModifierHelpText,
+  sharedWith: sharedWithCheckboxHelpText,
+  organisationsToSharedWith: organisationsToSharedWithHelpText,
+  organisation: organisationHelpText,
   supplier: supplierHelpText,
 };
