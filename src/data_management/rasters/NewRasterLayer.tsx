1c97b104aabe02eef90080b15711843bea5cdd63
hoanphungt <hoanphung.cpo@gmail.com>
2022-01-10 10:12:47 +0100

    Add async function to fetch layercollections to raster layer and wms layer forms

--- src/data_management/rasters/NewRasterLayer.tsx
+++ src/data_management/rasters/NewRasterLayer.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import RasterLayerForm from "./RasterLayerForm";
-import {
-  getLayercollections,
-} from "../../reducers";
-import SpinnerIfNotLoaded from '../../components/SpinnerIfNotLoaded';
-
 
 export const NewRasterLayer: React.FC = () => {
-  const layerCollections = useSelector(getLayercollections);
-
   return (
-    <SpinnerIfNotLoaded
-      loaded={layerCollections.isFetching === false}
-    >
-      <RasterLayerForm />
-    </SpinnerIfNotLoaded>
-
+    <RasterLayerForm />
   );
 }
