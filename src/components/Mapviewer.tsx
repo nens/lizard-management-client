2f97642d520e3b21a2bd4ba6934027bb432282b0
hoanphungt <hoanphung.cpo@gmail.com>
2022-04-07 11:31:23 +0200

    in progress

--- src/components/Mapviewer.tsx
+++ src/components/Mapviewer.tsx
@@ -1,58 +1,60 @@
-import { useEffect, useRef } from 'react';
-import ReactMapGL, { Source, Layer, MapRef, RasterSource } from 'react-map-gl';
+import { useEffect, useRef, useState } from 'react';
+import ReactMapGL, { Source, Layer, MapRef } from 'react-map-gl';
 import { mapBoxAccesToken } from '../mapboxConfig';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { getPath, timestamps as initialTimestamps } from '../utils/getPath';
 
-const timestamps = [
-  '2021-02-02T10:00:00',
-  '2021-02-02T10:05:00',
-  '2021-02-02T10:10:00',
-  '2021-02-02T10:15:00',
-  '2021-02-02T10:20:00',
-  '2021-02-02T10:25:00',
-  '2021-02-02T10:30:00',
-  '2021-02-02T10:35:00',
-  '2021-02-02T10:40:00',
-  '2021-02-02T10:45:00',
-  '2021-02-02T10:50:00',
-  '2021-02-02T10:55:00',
-  '2021-02-02T11:00:00'
-];
+// number of frame count based on the number of time steps
+const frameCount = initialTimestamps.length;
+
+// keep track of counter id in global scope to stop the setInterval
+let counterId: number;
 
 export default function MapViewer () {
   const mapRef = useRef<MapRef>(null);
-  const frameCount = 13;
-  let currentStep = 0;
+  const [step, setStep] = useState<number>(0);
+  const [tilesReady, setTilesReady] = useState<boolean>(false);
+  const [timestamps, setTimestamps] = useState<{time: string, loaded: boolean}[]>(initialTimestamps);
+  const [playAnimation, setPlayAnimation] = useState<boolean>(false);
 
-  const getPath = () => {
-    return `/wms/?service=WMS&request=GetMap&version=1.1.1&format=image/png&layers=radar:5min&styles=radar-5min&transparent=false&height=256&width=256&srs=EPSG:3857&time=${timestamps[currentStep]}&bbox={bbox-epsg-3857}`
-  }
-
-  // const getPath = () => {
-  //   return `https://docs.mapbox.com/mapbox-gl-js/assets/radar${currentImage}.gif`;
-  // };
-
-  // useEffect(() => {
-  //   fetch(`/wms/?service=WMS&request=GetMap&version=1.1.1&format=image/png&layers=radar:hour&styles=radar-hour&transparent=false&height=256&width=256&srs=EPSG:3857&time=2021-01-22T10:50:00&BBOX=469629.1017841229,6418264.3910496775,626172.1357121639,6574807.4249777235`)
-  //     .then(res => console.log(res))
-  // }, [])
+  // Functions to stop and start the animation
+  const startAnimation = () => {
+    setPlayAnimation(true);
+    counterId = window.setInterval(() => setStep((step) => (step + 1) % frameCount), 750);
+  };
+  const stopAnimation = () => {
+    setPlayAnimation(false);
+    window.clearInterval(counterId);
+  };
 
   useEffect(() => {
-    setInterval(() => {
-      currentStep = (currentStep + 1) % frameCount;
-      const source = mapRef.current!.getSource('wms') as RasterSource;
-      if (source) source.tiles = [getPath()]
-    }, 200);
-  })
+    const unloadedTiles = timestamps.filter(ts => !ts.loaded).length;
+    if (unloadedTiles === 0) {
+      setTilesReady(true);
+    } else {
+      setTilesReady(false);
+    }
+  }, [timestamps])
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     currentStep = (currentStep + 1) % frameCount;
+  //     const source = mapRef.current!.getSource('wms') as RasterSource;
+  //     if (source) source.tiles = [getPath()]
+  //   }, 200);
+  // })
 
   return (
     <div
       style={{
-        position: "fixed",
+        position: 'absolute',
         top: 0,
         left: 0,
-        width: "100vw",
-        height: "100vh",
+        width: '100%',
+        height: '100%',
+        display: 'grid',
+        gridTemplateRows: '14fr 1fr',
+        rowGap: 10
       }}
     >
       <ReactMapGL
@@ -70,45 +72,59 @@ export default function MapViewer () {
         mapStyle={"mapbox://styles/nelenschuurmans/ck8sgpk8h25ql1io2ccnueuj6"}
         // mapStyle={'mapbox://styles/mapbox/dark-v10'}
       >
-          {/* <Source
-            id={'radar'}
-            type={'image'}
-            url={getPath()}
-            coordinates={[
-              [-80.425, 46.437],
-              [-71.516, 46.437],
-              [-71.516, 37.936],
-              [-80.425, 37.936]
-            ]}
-          >
-              <Layer
-                id='radar-layer'
-                type='raster'
-                source={'radar'}
-                paint={{
-                  "raster-fade-duration": 0
-                }}
-              />
-          </Source> */}
-
-          <Source
-            id='wms'
-            type='raster'
-            tiles={[
-              getPath()
-            ]}
-            tileSize={256}
-          >
+        {/* <Source
+          id={'radar'}
+          type={'image'}
+          url={getPath()}
+          coordinates={[
+            [-80.425, 46.437],
+            [-71.516, 46.437],
+            [-71.516, 37.936],
+            [-80.425, 37.936]
+          ]}
+        >
             <Layer
-              id='wms-layer'
+              id='radar-layer'
               type='raster'
-              source={'wms'}
+              source={'radar'}
               paint={{
                 "raster-fade-duration": 0
               }}
             />
-          </Source>
+        </Source> */}
+
+        <Source
+          id='wms'
+          type='raster'
+          tiles={timestamps.map(ts => getPath(ts.time))}
+          tileSize={1024}
+        >
+          <Layer
+            id='wms-layer'
+            type='raster'
+            source={'wms'}
+            paint={{
+              "raster-opacity": 1
+            }}
+          />
+        </Source>
       </ReactMapGL>
+      <div>
+        <button
+          onClick={() => startAnimation()}
+          disabled={!tilesReady || playAnimation}
+        >
+          {tilesReady ? 'Play' : 'Loading ...'}
+        </button>
+        {' '}
+        <button
+          onClick={() => stopAnimation()}
+        >
+          Stop
+        </button>
+        {' '}
+        <span>Time: {timestamps[step].time}</span>
+      </div>
     </div>
   )
 }
